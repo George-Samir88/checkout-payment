f5c0be4f7c1e1312236a87821648153c3fb1717f
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-11-30 02:25:42 +0200

    add svg photo and continuous app bar

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:checkout_payment/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -8,7 +9,11 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        leading: Center(child: SvgPicture.asset('assets/images/arrow.svg')),
         title: const Text(
+
           'My Cart',
           textAlign: TextAlign.center,
           style: FontStyles.fontStyle25,
