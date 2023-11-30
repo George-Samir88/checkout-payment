d57ae49df35d4f4af723762bfeffaa779a8b25a5
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-11-30 02:04:32 +0200

    add styles and app bar

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+abstract class FontStyles {
+  static const fontStyle25 = TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static const fontStyleNormal18 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static const fontStyleSemiBold18 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const fontStyle20 = TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const fontStyle24 = TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const fontStyle22 = TextStyle(
+    color: Colors.black,
+    fontSize: 22,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+}
