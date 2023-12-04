1f876d9dbb99d504cbcfe93322f0289cedee4014
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-12-04 04:59:39 +0200

    add payment method item

--- lib/features/checkout/presentation/views/widgets/payment_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_view_body.dart
@@ -1,43 +1,28 @@
+import 'package:checkout_payment/features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class PaymentViewBody extends StatelessWidget {
   const PaymentViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
-        Container(
-          width: 103,
-          height: 62,
-          decoration: ShapeDecoration(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(width: 1.50, color: Color(0xFF34A853)),
-              borderRadius: BorderRadius.circular(15),
+        SizedBox(
+          height: 32.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            PaymentMethodItem(
+              isActive: false,
+              imagePath: 'assets/images/credit.svg',
             ),
-            shadows: const [
-              BoxShadow(
-                //belong to the color of the internal box
-                color: Color(0xFF34A853),
-                //belong to the blur of the internal box
-                blurRadius: 4,
-                //the same but the offset is the dimension with regards to x and y dimension
-                offset: Offset(0, 0),
-                //radius of the internal box
-                spreadRadius: 0,
-              )
-            ],
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
+            PaymentMethodItem(
+              isActive: true,
+              imagePath: 'assets/images/paypal.svg',
             ),
-            child: Center(
-                child:
-                    SvgPicture.asset('assets/images/credit.svg', height: 32)),
-          ),
+          ],
         ),
       ],
     );
