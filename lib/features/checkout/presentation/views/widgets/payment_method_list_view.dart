fb053647a37071c90f879ed5c5cfb0fd1cd6418a
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-12-04 05:27:56 +0200

    make payment method item clickable and toggle between active and inactive

--- lib/features/checkout/presentation/views/widgets/payment_method_list_view.dart
+++ lib/features/checkout/presentation/views/widgets/payment_method_list_view.dart
@@ -1,13 +1,20 @@
 import 'package:checkout_payment/features/checkout/presentation/views/widgets/payment_method_item.dart';
 import 'package:flutter/cupertino.dart';
 
-class PaymentMethodsListView extends StatelessWidget {
+class PaymentMethodsListView extends StatefulWidget {
   const PaymentMethodsListView({super.key});
 
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
   final List<String> imagesPaths = const [
     'assets/images/credit.svg',
     'assets/images/paypal.svg',
   ];
+
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +26,15 @@ class PaymentMethodsListView extends StatelessWidget {
             padding: const EdgeInsets.symmetric(
               horizontal: 16.0,
             ),
-            child: PaymentMethodItem(
-              imagePath: imagesPaths[index],
+            child: GestureDetector(
+              onTap: () {
+                activeIndex = index;
+                setState(() {});
+              },
+              child: PaymentMethodItem(
+                imagePath: imagesPaths[index],
+                isActive: activeIndex == index,
+              ),
             ),
           );
         },
