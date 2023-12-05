e097f14084c95c24663324f6257b7f8fdfc25fc0
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-12-05 15:38:00 +0200

    add body of thank view and circle from left and right

--- lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 
+//custom painter allowed
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 40,
+        bottom: 20,
+        right: 20,
+        left: 20,
+      ),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              color: const Color(0xFFD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Positioned(
+            left: -20,
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            child: Container(
+              width: 35,
+              height: 35,
+              decoration: const ShapeDecoration(
+                color: Colors.white,
+                shape: OvalBorder(),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -20,
+            bottom: MediaQuery.sizeOf(context).height * 0.2,
+            child: Container(
+              width: 35,
+              height: 35,
+              decoration: const ShapeDecoration(
+                color: Colors.white,
+                shape: OvalBorder(),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
