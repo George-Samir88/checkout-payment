42247f0477ad0b24df69ab818fd3a392b08021ad
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-12-05 14:08:13 +0200

    refactor the behavior payment details view body to change from singleChildScrollView to CustomScrollView

--- lib/features/checkout/presentation/views/widgets/payment_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_view_body.dart
@@ -4,36 +4,49 @@ import 'package:flutter/material.dart';
 
 import 'custom_credit_card.dart';
 
-class PaymentViewBody extends StatelessWidget {
+class PaymentViewBody extends StatefulWidget {
   const PaymentViewBody({super.key});
 
   @override
+  State<PaymentViewBody> createState() => _PaymentViewBodyState();
+}
+
+class _PaymentViewBodyState extends State<PaymentViewBody> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20.0,
+    return CustomScrollView(
+      slivers: [
+        const SliverToBoxAdapter(
+          child: SizedBox(
+            height: 32.0,
+          ),
         ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 32.0,
-            ),
-            const PaymentMethodsListView(),
-            const CustomCreditCard(),
-            const SizedBox(
-              height: 35.0,
-            ),
-            CustomButton(
-              onTap: () {},
-              text: 'Complete Payment',
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-          ],
+        const SliverToBoxAdapter(child: PaymentMethodsListView()),
+        SliverToBoxAdapter(
+          child: CustomCreditCard(
+            formKey: formKey,
+          ),
         ),
-      ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              bottom: 12.0,
+              left: 20.0,
+              right: 20.0,
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: CustomButton(
+                onTap: () {},
+                text: 'Complete Payment',
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
