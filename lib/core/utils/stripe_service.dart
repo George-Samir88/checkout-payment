8e0c2be2bea8d7ecd418c1f587076868ac4c73c5
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-01-30 17:14:00 +0200

    create stripe service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,20 @@
-class StripeService {
+import 'package:checkout_payment/core/utils/api_keys.dart';
+import 'package:checkout_payment/core/utils/api_service.dart';
+import 'package:checkout_payment/features/checkout/data/models/payment_intent_object_input/payment_intent_object_input.dart';
+import 'package:checkout_payment/features/checkout/data/models/payment_intent_object_response/payment_intent_object.dart';
 
+class StripeService {
+  ApiService apiService = ApiService();
+  final String baseUrl = 'https://api.stripe.com';
+
+  Future<PaymentIntentObject> createPaymentIntent(
+      {required PaymentIntentObjectInput paymentIntentObjectInput}) async {
+    var response = await apiService.post(
+        token: ApiKeys.secretKey,
+        body: paymentIntentObjectInput.toJson(),
+        url: '$baseUrl/v1/payment_intents');
+    PaymentIntentObject paymentIntentObject =
+        PaymentIntentObject.fromJson(response.data);
+    return paymentIntentObject;
+  }
 }
--- lib/notes.dart
+++ lib/notes.dart
@@ -4,3 +4,8 @@
 2)init paymentSheet(merchantName , paymentClientSecretKey)
 3)presentPaymentSheet
 */
+/*
+-------------delete pushed changes-------------
+git reset HEAD~1        //1 is the last change ,2 is the last 2 changes
+git push -f origin main
+*/
