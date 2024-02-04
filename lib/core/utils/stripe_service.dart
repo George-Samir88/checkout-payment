1fcf60932553dd3273b0eec8e892c6b054c6aa74
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-02-04 16:43:29 +0200

    implement create customer method in stripe service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,5 +1,6 @@
 import 'package:checkout_payment/core/utils/api_keys.dart';
 import 'package:checkout_payment/core/utils/api_service.dart';
+import 'package:checkout_payment/features/checkout/data/models/customer/customer/customer.dart';
 import 'package:checkout_payment/features/checkout/data/models/payment_intent_object_input/payment_intent_object_input.dart';
 import 'package:checkout_payment/features/checkout/data/models/payment_intent_object_response/payment_intent_object.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -7,6 +8,20 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 class StripeService {
   ApiService apiService = ApiService();
   final String baseUrl = 'https://api.stripe.com';
+
+  Future<Customer> createCustomer(
+      {required String name, required String email}) async {
+    var response = await apiService.post(
+      token: ApiKeys.secretKey,
+      body: {
+        'name': name,
+        'email': email,
+      },
+      url: '$baseUrl/v1/customers',
+    );
+    Customer customer = Customer.fromJson(response.data);
+    return customer;
+  }
 
   Future<PaymentIntentObject> createPaymentIntent(
       {required PaymentIntentObjectInput paymentIntentObjectInput}) async {
