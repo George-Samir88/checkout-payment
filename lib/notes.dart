08071be32c35a61a3ef244d791140c1814b12152
George-Samir88 <George-Samir88@example.com>
2024-02-10 19:55:22 +0200

    add some comments

--- lib/notes.dart
+++ lib/notes.dart
@@ -5,8 +5,19 @@
 2)init paymentSheet(merchantName , paymentClientSecretKey , ephemeralKeySecret)
 3)presentPaymentSheet()
 */
+
+
+
 /*
 -------------delete pushed changes-------------
 git reset HEAD~1        //1 is the last change ,2 is the last 2 changes
 git push -f origin main
+*/
+
+
+
+/*-----------for adding the same project repo on another enviornment-------------
+1)git clone https://github.com/xxxxxxx/personal_finance.git
+2)git config user.email "xxxxxxx@hotmail.com"
+3)git config user.name "xxxxxxx"
 */
