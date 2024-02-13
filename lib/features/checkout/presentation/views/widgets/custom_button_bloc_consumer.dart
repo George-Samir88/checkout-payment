60c208ee9cf2317207a5dd37266ad08e0a2be5ac
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-02-13 14:30:25 +0200

    create item list model

--- lib/features/checkout/presentation/views/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/presentation/views/widgets/custom_button_bloc_consumer.dart
@@ -65,13 +65,13 @@ class CustomButtonBlocConsumer extends StatelessWidget {
                         {
                           "name": "Apple",
                           "quantity": 4,
-                          "price": '10',
+                          "price": "10",
                           "currency": "USD"
                         },
                         {
                           "name": "Pineapple",
                           "quantity": 5,
-                          "price": '12',
+                          "price": "12",
                           "currency": "USD"
                         }
                       ],
